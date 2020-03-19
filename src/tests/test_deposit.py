91fb92d75ce4e0cfa650782dc5fc5428c69386ad
cyc60 <avsysoev60@gmail.com>
2020-03-19 22:44:48 +0300

    Add test for cloud balance after deposit

--- src/tests/test_deposit.py
+++ src/tests/test_deposit.py
@@ -22,8 +22,8 @@ class TestDeposit(VideocoinMixin):
         eth_private_key = self.get_initial_value(PRIVATE_KEY_METAMASK)
         timeout = 300
 
-        vid_contract = w3.eth.contract(self.get_initial_value(VID_TOKEN_ADDR), abi=abi)
         start_amt = self.get_vid_balance_of_erc20(w3, abi, eth_addr)
+        vid_contract = w3.eth.contract(self.get_initial_value(VID_TOKEN_ADDR), abi=abi)
         vid_txn = vid_contract.functions.transfer(
             user.wallet_address, deposit_amt
         ).buildTransaction(
@@ -35,7 +35,7 @@ class TestDeposit(VideocoinMixin):
                 'nonce': w3.eth.getTransactionCount(eth_addr),
             }
         )
-        signed_txn = w3.eth.account.signTransaction(vid_txn, eth_private_key)
+        signed_txn = w3.eth.account.sign_transaction(vid_txn, eth_private_key)
         w3.eth.sendRawTransaction(signed_txn.rawTransaction)
 
         start_time = datetime.now()
@@ -56,13 +56,19 @@ class TestDeposit(VideocoinMixin):
 
         assert start_amt - end_amt == deposit_amt
 
-    @pytest.mark.skip('in progress')
+    @pytest.mark.smoke
     def test_deposit_updates_vid_acct_balance_correctly(self, user, w3, abi):
         """
         Check correct erc20 token transfer
         """
-        start_amt = user.wallet_balance
+
         deposit_amt = 10 * 10 ** 18
+        eth_addr = self.get_initial_value(DEPOSIT_ADDRESS_METAMASK)
+        eth_private_key = self.get_initial_value(PRIVATE_KEY_METAMASK)
+        timeout = 100
+
+        start_amt = user.wallet_balance
+        logger.debug('Start VID balance: {:.6e}'.format(start_amt))
 
         vid_contract = w3.eth.contract(self.get_initial_value(VID_TOKEN_ADDR), abi=abi)
         vid_txn = vid_contract.functions.transfer(
@@ -73,25 +79,28 @@ class TestDeposit(VideocoinMixin):
                 'gas': 200000,
                 'value': 0,
                 'gasPrice': 1000000000,
-                'nonce': w3.eth.getTransactionCount(self.get_initial_value(DEPOSIT_ADDRESS_METAMASK)),
+                'nonce': w3.eth.getTransactionCount(eth_addr),
             }
         )
-        private_key = '523FC9D14D9610691BE281348968976FD397788EED49B1DB8949005ECA85B2E0'
-        signed_txn = w3.eth.account.signTransaction(vid_txn, private_key)
+        signed_txn = w3.eth.account.sign_transaction(vid_txn, eth_private_key)
         txn_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
-        print(txn_hash.hex())
 
         start_time = datetime.now()
-        sleep(300)
-        end_amt = user.wallet_balance
-
-        # while start_amt == end_amt:
-        #     end_amt = user.wallet_balance
-        #     sleep(1)
+        latest_amt = user.wallet_balance
+        while start_amt == latest_amt:
+            if utils.time_from_start(start_time) > timeout:
+                        raise RuntimeError(
+                            'Took too long to subtract funds. Timeout allowed: {}'.format(timeout)
+                        )
+            latest_amt = user.wallet_balance
+            logger.debug('End VID balance: {:.6e}'.format(latest_amt))
+            logger.debug('txn_hash.hex(): {}'.format(txn_hash.hex()))
+            sleep(5)
 
         logger.debug('Start VID balance: {:.6e}'.format(start_amt))
-        logger.debug('End VID balance: {:.6e}'.format(end_amt))
-        # logger.debug('Duration to receive funds: {}'.format(start_time - datetime.now()))
+        logger.debug('End VID balance: {:.6e}'.format(latest_amt))
+        logger.debug('Duration to subtract funds: {}'.format(datetime.now() - start_time))
+        assert start_amt - latest_amt == deposit_amt
 
     @pytest.mark.skip('in progress')
     def test_time_before_deposit_funds_received(self, user, w3, abi):
@@ -124,7 +133,7 @@ class TestDeposit(VideocoinMixin):
 
             while start_amt == end_amt:
                 end_amt = user.wallet_balance
-                sleep(1)
+                sleep(5)
 
             end_time = datetime.now()
             logger.debug('Start VID balance: {:.6e}'.format(start_amt))
